bc9fd000fcc55217a68970d5c960c8065be1fdf6
Lia Siti Sholihah <liasiti@1s-MacBook-Air.local>
2020-04-20 23:45:41 +0700

    update patch

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ const Hapi = require('hapi');
 const server = new Hapi.Server({ 
     host: 'localhost', 
     port: 3001, 
+    routes: {
+      cors: true
+   }
 });
 
 const { Pool } = require('pg')
